5b4770e796152118b311ac0c4067bc63a64a7342
Mukimut Salat <mukimut.salat@sebpo.com>
2024-09-03 20:43:51 +0600

    template added

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,15 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('It works on ' + port);
+});
+
+app.get('/:location', (req, res) => {
+    res.send('path is ' + req.params.location);
 });
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
 });
+
+function trace(t) {console.log(t)}
